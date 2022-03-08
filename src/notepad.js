fdd04adf4e8614b78ee57c5653a3d37156b508db
LOSTCANTiM <68940014+LOSTCANTiM@users.noreply.github.com>
2022-03-08 09:13:28 +0530

    finished

--- src/notepad.js
+++ src/notepad.js
@@ -4,18 +4,23 @@ const fs = require('fs');
 var lines = 100;
 
 document.addEventListener('keydown', function (key) {
+    fs.readFile("test.txt", (err, data) => {
+        if (document.getElementById('txtarea').value.toString() == data.toString()) {
+            document.getElementById("sts").innerHTML = "<i class='fa-regular fa-circle-check'></i> Saved!";
+        }
+        else {
+            document.getElementById("sts").innerHTML = "<i class='fa-regular fa-circle'></i> Unsaved work.";
+        }
+        if (err) throw err;
+        console.log(data.toString());
+    });
+
     if (key.code == "Space" || key.code == "Enter") {
         var e = document.getElementById('txtarea');
         lines++;
         e.rows = lines;
     }
-
-    console.log(key.code);
 });
-
-// function writeFile (data) {
-//     console.log(data);
-// }
 
 window.onload = function () {
     document.getElementById("minimizeBtn").addEventListener('click', () => {
@@ -28,8 +33,26 @@ window.onload = function () {
 
     document.getElementById('saveBtn').addEventListener('click', () => {
         var value = document.getElementById('txtarea').value;
-        fs.writeFile("test.txt", value.toString(), (err) => {
+        fs.writeFile("temp.txt", value.toString(), (err) => {
             if (err) throw err;
         });
+    
+        var saveLink = document.createElement('a');
+        saveLink.setAttribute('href', 'temp.txt');
+        saveLink.setAttribute('download', 'file.txt');
+        saveLink.click();
+
+        fs.readFile("temp.txt", (err, data) => {
+            if (document.getElementById('txtarea').value.toString() == data.toString()) {
+                document.getElementById("sts").innerHTML = "<i class='fa-regular fa-circle-check'></i> Saved!";
+            }
+            else {
+                document.getElementById("sts").innerHTML = "<i class='fa-regular fa-circle'></i> Unsaved work.";
+            }
+            if (err) throw err;
+            console.log(data.toString());
+        });
+
     });
+    
 };
